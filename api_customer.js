91fdd583c662caf6464a82c0da4dadf965e169dc
thanaphon0737 <35342731+thanaphon0737@users.noreply.github.com>
2020-12-25 21:54:14 +0700

    add verify customerId

--- api_customer.js
+++ api_customer.js
@@ -8,53 +8,72 @@ router.get("/customer", async (req, res) => {
     res.json(result);
 });
 
-router.get("/table", async(req, res) =>{
+router.get("/table", async (req, res) => {
     const result = await table.findAll();
     res.json(result)
 })
 
-router.post("/table", async(req, res)=>{
-    try{
-        const result = await table.create({capacity:req.body.capacity});
+router.post("/table", async (req, res) => {
+    try {
+        const result = await table.create({ capacity: req.body.capacity });
         res.json({ result: constants.kResultOk, message: JSON.stringify(result) });
-    }catch(error){
+    } catch (error) {
         res.json({ result: constants.kResultNok, message: JSON.stringify(error) });
     }
 })
 router.post("/customer", async (req, res) => {
-    
+
     try {
         let data = {
-            customer_name:req.body.customer_name,
-            table_id:req.body.table_id
+            customer_name: req.body.customer_name,
+            table_id: req.body.table_id
         }
         const result = await customer.create(data);
-        
+
         res.json({ result: constants.kResultOk, message: JSON.stringify(result) });
-        
+
     } catch (error) {
-        
+
         res.json({ result: constants.kResultNok, message: JSON.stringify(error) });
     }
 });
 
-router.put("/customer", async(req, res) => {
+router.put("/customer", async (req, res) => {
     console.log(req.body)
     const data = {
         customer_name: req.body.customer_name,
         table_id: req.body.table_id
     }
-    try{
-        let result = await customer.update(data, {where:{id: req.body.id}});
+    try {
+        let result = await customer.update(data, { where: { id: req.body.id } });
         res.json({
             result: constants.kResultOk,
             message: result
         })
-    }catch(error){
+    } catch (error) {
         res.json(400).json(error)
     }
 })
-router.get('/customer/:id', (req,res)=> {
-    res.end(`this is table${req.params.id}`);
+router.get('/customer/:id', async (req, res) => {
+    try {
+
+        let customerId = await customer.findOne({ where: { id: req.params.id } })
+        if (customerId) {
+
+            res.json({
+                result: constants.kResultOk,
+                message: customerId
+            })
+        }else {
+            res.json({
+                result: constants.kResultNok,
+                message: 'cant find customer id'
+            })
+        }
+
+    } catch (er) {
+        res.json(400)
+    }
+
 })
 module.exports = router;
